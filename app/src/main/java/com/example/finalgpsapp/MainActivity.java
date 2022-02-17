ca93aa503984a813196956f65bd442801db831e5
Kushaal Latupalli <klatupalli@gmail.com>
2022-02-17 00:05:22 -0500

    GPS app

--- app/src/main/java/com/example/finalgpsapp/MainActivity.java
+++ app/src/main/java/com/example/finalgpsapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.finalgpsapp;
 
+import static android.os.SystemClock.elapsedRealtime;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -12,6 +13,9 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 import java.io.IOException;
@@ -26,7 +30,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     TextView dis;
     LocationManager lm;
     String currentAddy;
-    Geocoder geocoder;
+    Geocoder gc;
     double distance = 0;
     List<Location> locList = new ArrayList();
     List<Address> addyList = new ArrayList();
@@ -41,14 +45,12 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         dis = findViewById(R.id.textViewDistance);
 
         if ((ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) &&(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED))
-        {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
-        }
         else
         {
             lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
             lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1,1,MainActivity.this);
-            geocoder = new Geocoder(MainActivity.this, Locale.getDefault());
+            gc = new Geocoder(MainActivity.this, Locale.getDefault());
         }
     }
     @Override
@@ -60,14 +62,14 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
             {
                 if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) )
                 {
-                    Toast.makeText(MainActivity.this, "GPS Permission Granted", Toast.LENGTH_LONG).show();
+                    Toast.makeText(MainActivity.this, "GPS Permission Granted", Toast.LENGTH_SHORT).show();
                     lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
                     lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1,1,MainActivity.this);
-                    geocoder = new Geocoder(MainActivity.this, Locale.getDefault());
+                    gc = new Geocoder(MainActivity.this, Locale.getDefault());
                 }
             }
             else
-                Toast.makeText(this, "GPS Permission Denied", Toast.LENGTH_LONG).show();
+                Toast.makeText(this, "GPS Permission Denied", Toast.LENGTH_SHORT).show();
             return;
         }
     }
@@ -75,23 +77,20 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     public void onLocationChanged(@NonNull Location location) {
         double latitude = location.getLatitude();
         double longitude = location.getLongitude();
-        lat.setText(String.valueOf(latitude));
-        lon.setText(String.valueOf(longitude));
-
+        lat.setText("Latitude:\n"+latitude);
+        lon.setText("Longitude:\n"+longitude);
+        lon.setGravity(View.TEXT_ALIGNMENT_CENTER);
         locList.add(location);
-
         if(locList.size() > 1)
         {
             Location currentLocation = locList.get(locList.size()-2);
             Location newLocation = locList.get(locList.size()-1);
-
             distance += currentLocation.distanceTo(newLocation);
             double roundDistance = Math.round(distance*100.0)/100.0;
-            dis.setText(String.valueOf(roundDistance) + " Meters");
+            dis.setText("Distance:\n"+roundDistance + " Meters");
         }
-
         try {
-            addyList = geocoder.getFromLocation(latitude,longitude,1);
+            addyList = gc.getFromLocation(latitude,longitude,1);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -102,12 +101,11 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
             {
                 currentAddy = userAddress.getAddressLine(i);
             }
-            addy.setText(currentAddy);
+            addy.setText("Address:\n"+currentAddy);
         }
         else
-        {
-            Toast.makeText(MainActivity.this, "No Selected Address", Toast.LENGTH_SHORT).show();
-        }
+            Toast.makeText(MainActivity.this, "No Address", Toast.LENGTH_SHORT).show();
     }
+
 
 }
